a525c28caf803e8e2b1381ef5107bfa36baa4c92
MUNAZIR <60435223+Munazirraza@users.noreply.github.com>
2023-03-30 15:10:05 +0530

    init

---
+++ maze_solver1/src/main/java/com/mycompany/maze_solver1/Maze_solver1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maze_solver1;
+
+/**
+ *
+ * @author Munazir
+ */
+public class Maze_solver1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
